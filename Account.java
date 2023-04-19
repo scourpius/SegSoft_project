22bc60c0da84646dea29aab04086ffe8f6ce9fb3
Alexandre Godinho <ams.godinho@campus.fct.unl.pt>
2023-04-19 01:38:47 +0100

    Account methods

--- Account.java
+++ Account.java
@@ -3,7 +3,46 @@ public class Account{
     private String username, password;
     private boolean logged_in, locked;
     
-    public Account(){
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.logged_in = false;
+        this.locked = true; //?
+    }
 
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public boolean isLogged(){
+        return this.logged_in;
+    }
+
+    public boolean isLocked(){
+        return this.locked;
+    }
+
+    public void login(){
+        this.logged_in = true;
+    }
+
+    public void logout(){
+        this.logged_in = false;
+    }
+
+    public void lock(){
+        this.locked = true;
+    }
+
+    public void unlock(){
+        this.locked = false;
+    }
+
+    public void change_pwd(String newPwd){
+        this.password = newPwd;
     }
 }
